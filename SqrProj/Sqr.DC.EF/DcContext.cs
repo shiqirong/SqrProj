2e4e7a85b493af5980c2921680393802f3bf953e
shiqr <shiqr@zhongan.com.cn>
2018-01-19 14:35:57 +0800

    entityframework

---
+++ SqrProj/Sqr.DC.EF/DcContext.cs
@@ -0,0 +1,21 @@
+﻿using MySql.Data.Entity;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+
+namespace Sqr.DC.EF
+{
+    [DbConfigurationType(typeof(MySqlEFConfiguration))]
+    public class DcContext: DbContext
+    {
+        public DcContext() : this("DC") { }
+        public DcContext(string connectionKey)
+            : base(string.Format("name={0}", connectionKey))
+        {
+            //Database.SetInitializer(new EFInitializer());
+            Database.SetInitializer<DcContext>(null);
+        }
+    }
+}
